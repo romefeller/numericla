9c3f47d4ab29cf51be29c0ccc05da41e34a89811
romefeller <alexgrcol@hotmail.com>
2022-06-14 09:26:55 -0300

    changing names

--- default.nix
+++ default.nix
@@ -1,15 +1,4 @@
-{ mkDerivation, base, bytestring, cassava, gnuplot, hmatrix
-, hmatrix-csv, lib
-}:
-mkDerivation {
-  pname = "numericla";
-  version = "0.1.0.0";
-  src = ./.;
-  isLibrary = false;
-  isExecutable = true;
-  executableHaskellDepends = [
-    base bytestring cassava gnuplot hmatrix hmatrix-csv
-  ];
-  license = "unknown";
-  hydraPlatforms = lib.platforms.none;
-}
+let
+ pkgs = import <nixpkgs> { };
+in
+ pkgs.haskellPackages.callPackage ./release.nix { }
--- release.nix
+++ release.nix
@@ -1,4 +1,15 @@
-let
- pkgs = import <nixpkgs> { };
-in
- pkgs.haskellPackages.callPackage ./default.nix { }
+{ mkDerivation, base, bytestring, cassava, gnuplot, hmatrix
+, hmatrix-csv, lib
+}:
+mkDerivation {
+  pname = "numericla";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    base bytestring cassava gnuplot hmatrix hmatrix-csv
+  ];
+  license = "unknown";
+  hydraPlatforms = lib.platforms.none;
+}
